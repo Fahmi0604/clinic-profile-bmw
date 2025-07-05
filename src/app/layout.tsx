5e52efb0757e0744f2f390ffadc524a45dc403db
fahmi0604 <fahmifirmansyah917@gmail.com>
2025-07-05 09:11:38 +0700

    update google analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { CustomFonts } from "./fonts/custom-fonts";
 import { getSettings } from "@/lib/api";
+import Script from "next/script";
 // import { PageWrapper } from "@/components";
 
 const outfit = Outfit({
@@ -23,9 +24,43 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Z67MFBT9H0');
+          `}
+          {/* Google Tag Manager */}
+          <Script id="gtm-head" strategy="afterInteractive">
+            {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PBXFJRV5');
+          `}
+          </Script>
+        </Script>
+      </head>
       <body
         className={`${outfit.variable} ${CustomFonts.variable} antialiased bg-white font-outfit`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PBXFJRV5"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Navbar settings={settings.data} />
         <main className="min-h-screen">
           {children}
