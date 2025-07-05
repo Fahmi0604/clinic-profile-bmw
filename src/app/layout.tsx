f9ab216499ed4917d56a130900ac236e68fb4773
fahmi0604 <fahmifirmansyah917@gmail.com>
2025-07-05 09:41:34 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default async function RootLayout({
       <head>
         {/* Google Analytics Script */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+          src="https://www.googletagmanager.com/gtag/js?id=G-Z67MFBT9H0"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
@@ -37,16 +37,16 @@ export default async function RootLayout({
             gtag('js', new Date());
             gtag('config', 'G-Z67MFBT9H0');
           `}
-          {/* Google Tag Manager */}
-          <Script id="gtm-head" strategy="afterInteractive">
-            {`
+        </Script>
+        {/* Google Tag Manager */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-PBXFJRV5');
           `}
-          </Script>
         </Script>
       </head>
       <body
